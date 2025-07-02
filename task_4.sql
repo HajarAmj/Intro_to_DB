4594907557bea7a899c8dfc93d5db221cd126021
HajarAmj <96659034+HajarAmj@users.noreply.github.com>
2025-07-02 01:41:24 +0100

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,15 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Show the full description of the books table
-SHOW COLUMNS FROM Books;
+-- Show the full description of the Books table using INFORMATION_SCHEMA
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
